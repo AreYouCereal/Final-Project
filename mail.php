b058aa1b6572d3778560e790c766e2342f3f2577
AreYouCereal <Wendi@CreateSalesAndMarketing.com>
2014-11-11 12:27:23 -0600

    final content and form edits for php file

---
+++ mail.php
@@ -0,0 +1,16 @@
+
+
+`
+
+<?php
+$name = $_POST['name'];
+$email = $_POST['email'];
+$type = $_POST['type'];
+$message = $_POST['message'];
+$formcontent=" From: $name \n Type: $type \n Message: $message";
+$recipient = "WENDI@CREATESALESANDMARKETING.COM";
+$subject = "Contact Form";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You!" . " -" . "<a href='form.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
+?>
